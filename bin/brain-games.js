0e252d76baebbf7d0b8189169b8a09cb431d20b1
Anna Tikhontsova <palshina.anyaa@gmail.com>
2022-02-16 14:23:55 +0300

    fix brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import printName from '../src/cli.js';
+import ask from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-console.log(`Hello,  ${printName()}`);
+const name = ask('May I have your name? ');
+console.log(`Hello, ${name}!` );
